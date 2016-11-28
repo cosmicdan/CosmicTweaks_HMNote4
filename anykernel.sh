8c9d18538e08cea8e3dc392cbc703b4a38b69aaf
CosmicDan <dconnolly.au@gmail.com>
2016-11-29 01:55:13 +1100

    Initial push of v2016.11.26

---
+++ anykernel.sh
@@ -0,0 +1,306 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# EDIFY properties
+kernel.string=MIUI8 CosmicTweaks
+do.devicecheck=1
+do.initd=0
+do.modules=0
+do.cleanup=1
+device1=nikel
+
+# shell variables
+block=/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/boot;
+
+## end setup
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# set up extracted files and directories
+ramdisk=/tmp/anykernel/ramdisk;
+bin=/tmp/anykernel/tools;
+split_img=/tmp/anykernel/split_img;
+patch=/tmp/anykernel/patch;
+
+chmod -R 755 $bin;
+mkdir -p $ramdisk $split_img;
+
+OUTFD=/proc/self/fd/$1;
+
+# ui_print <text>
+ui_print() { echo -e "ui_print $1\nui_print" > $OUTFD; }
+
+# contains <string> <substring>
+contains() { test "${1#*$2}" != "$1" && return 0 || return 1; }
+
+# dump boot and extract ramdisk
+dump_boot() {
+  dd if=$block of=/tmp/anykernel/boot.img;
+  $bin/unpackbootimg -i /tmp/anykernel/boot.img -o $split_img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Dumping/splitting image failed. Aborting..."; exit 1;
+  fi;
+  mv -f $ramdisk /tmp/anykernel/rdtmp;
+  mkdir -p $ramdisk;
+  cd $ramdisk;
+  gunzip -c $split_img/boot.img-ramdisk.gz | cpio -i;
+  if [ $? != 0 -o -z "$(ls $ramdisk)" ]; then
+    ui_print " "; ui_print "Unpacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  cp -af /tmp/anykernel/rdtmp/* $ramdisk;
+}
+
+# repack ramdisk then build and write image
+write_boot() {
+  cd $split_img;
+  cmdline=`cat *-cmdline`;
+  board=`cat *-board`;
+  base=`cat *-base`;
+  pagesize=`cat *-pagesize`;
+  kerneloff=`cat *-kerneloff`;
+  ramdiskoff=`cat *-ramdiskoff`;
+  tagsoff=`cat *-tagsoff`;
+  if [ -f *-second ]; then
+    second=`ls *-second`;
+    second="--second $split_img/$second";
+    secondoff=`cat *-secondoff`;
+    secondoff="--second_offset $secondoff";
+  fi;
+  if [ -f /tmp/anykernel/zImage ]; then
+    kernel=/tmp/anykernel/zImage;
+  elif [ -f /tmp/anykernel/zImage-dtb ]; then
+    kernel=/tmp/anykernel/zImage-dtb;
+  else
+    kernel=`ls *-zImage`;
+    kernel=$split_img/$kernel;
+  fi;
+  if [ -f /tmp/anykernel/dtb ]; then
+    dtb="--dt /tmp/anykernel/dtb";
+  elif [ -f *-dtb ]; then
+    dtb=`ls *-dtb`;
+    dtb="--dt $split_img/$dtb";
+  fi;
+  if [ -f "$bin/mkbootfs" ]; then
+    $bin/mkbootfs /tmp/anykernel/ramdisk | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  else
+    cd $ramdisk;
+    find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gz;
+  fi;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking ramdisk failed. Aborting..."; exit 1;
+  fi;
+  $bin/mkbootimg --kernel $kernel --ramdisk /tmp/anykernel/ramdisk-new.cpio.gz $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
+  if [ $? != 0 ]; then
+    ui_print " "; ui_print "Repacking image failed. Aborting..."; exit 1;
+  elif [ `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+    ui_print " "; ui_print "New image larger than boot partition. Aborting..."; exit 1;
+  fi;
+  if [ -f "/data/custom_boot_image_patch.sh" ]; then
+    ash /data/custom_boot_image_patch.sh /tmp/anykernel/boot-new.img;
+    if [ $? != 0 ]; then
+      ui_print " "; ui_print "User script execution failed. Aborting..."; exit 1;
+    fi;
+  fi;
+  dd if=/tmp/anykernel/boot-new.img of=$block;
+}
+
+# backup_file <file>
+backup_file() { cp $1 $1~; }
+
+# replace_string <file> <if search string> <original string> <replacement string>
+replace_string() {
+  if [ -z "$(grep "$2" $1)" ]; then
+      sed -i "s;${3};${4};" $1;
+  fi;
+}
+
+# replace_section <file> <begin search string> <end search string> <replacement string>
+replace_section() {
+  begin=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+  for end in `grep -n "$3" $1 | cut -d: -f1`; do
+    if [ "$begin" -lt "$end" ]; then
+      sed -i "/${2//\//\\/}/,/${3//\//\\/}/d" $1;
+      sed -i "${begin}s;^;${4}\n;" $1;
+      break;
+    fi;
+  done;
+}
+
+# remove_section <file> <begin search string> <end search string>
+remove_section() {
+  begin=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+  for end in `grep -n "$3" $1 | cut -d: -f1`; do
+    if [ "$begin" -lt "$end" ]; then
+      sed -i "/${2//\//\\/}/,/${3//\//\\/}/d" $1;
+      break;
+    fi;
+  done;
+}
+
+# insert_line <file> <if search string> <before|after> <line match string> <inserted line>
+insert_line() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;${5}\n;" $1;
+  fi;
+}
+
+# replace_line <file> <line replace string> <replacement line>
+replace_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+    sed -i "${line}s;.*;${3};" $1;
+  fi;
+}
+
+# remove_line <file> <line match string>
+remove_line() {
+  if [ ! -z "$(grep "$2" $1)" ]; then
+    line=`grep -n "$2" $1 | head -n1 | cut -d: -f1`;
+    sed -i "${line}d" $1;
+  fi;
+}
+
+# prepend_file <file> <if search string> <patch file>
+prepend_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo "$(cat $patch/$3 $1)" > $1;
+  fi;
+}
+
+# insert_file <file> <if search string> <before|after> <line match string> <patch file>
+insert_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    case $3 in
+      before) offset=0;;
+      after) offset=1;;
+    esac;
+    line=$((`grep -n "$4" $1 | head -n1 | cut -d: -f1` + offset));
+    sed -i "${line}s;^;\n;" $1;
+    sed -i "$((line - 1))r $patch/$5" $1;
+  fi;
+}
+
+# append_file <file> <if search string> <patch file>
+append_file() {
+  if [ -z "$(grep "$2" $1)" ]; then
+    echo -ne "\n" >> $1;
+    cat $patch/$3 >> $1;
+    echo -ne "\n" >> $1;
+  fi;
+}
+
+# replace_file <file> <permissions> <patch file>
+replace_file() {
+  cp -pf $patch/$3 $1;
+  chmod $2 $1;
+}
+
+# patch_fstab <fstab file> <mount match name> <fs match type> <block|mount|fstype|options|flags> <original string> <replacement string>
+patch_fstab() {
+  entry=$(grep "$2" $1 | grep "$3");
+  if [ -z "$(echo "$entry" | grep "$6")" ]; then
+    case $4 in
+      block) part=$(echo "$entry" | awk '{ print $1 }');;
+      mount) part=$(echo "$entry" | awk '{ print $2 }');;
+      fstype) part=$(echo "$entry" | awk '{ print $3 }');;
+      options) part=$(echo "$entry" | awk '{ print $4 }');;
+      flags) part=$(echo "$entry" | awk '{ print $5 }');;
+    esac;
+    newentry=$(echo "$entry" | sed "s;${part};${6};");
+    sed -i "s;${entry};${newentry};" $1;
+  fi;
+}
+
+## end methods
+
+
+
+## AnyKernel permissions
+# set permissions for included files
+#chmod -R 755 $ramdisk
+#chmod 644 $ramdisk/res/synapse/*
+#chmod -R 755 $ramdisk/res/synapse/actions
+
+## AnyKernel install
+#dump_boot;
+
+# begin ramdisk changes
+
+# init.tuna.rc
+#append_file init.tuna.rc "/sbin/uci" init.tuna;
+
+# end ramdisk changes
+
+#write_boot;
+
+## end install
+ui_print " ";
+ui_print "[#] Erasing /cust (nothing but junk here)...";
+mount /cust
+rm -rf /cust/*
+umount /cust
+
+ui_print "[#] /system removals...";
+rm -rf /system/data-app/*;
+ui_print "    [i] Erased Chinese bloat at /system/data-app/*";
+rm /system/bin/install-recovery.sh;
+rm /system/recovery-from-boot.p;
+ui_print "    [i] Deleted stock recovery patch & script";
+rm -rf /system/app/AnalyticsCore;
+ui_print "    [i] Removed app/AnalyticsCore (Phone-home backdoor app)";
+rm -rf /system/app/AutoTest;
+ui_print "    [i] Removed app/AutoTest (Engineering diagnostics)";
+rm -rf /system/app/SogouInput;
+ui_print "    [i] Removed app/SogouInput (Chinese IME)";
+rm -rf /system/app/Whetstone
+ui_print "    [i] Removed Whetstone (appkiller)";
+rm -rf /system/app/AMAPNetworkLocation;
+# Removing causes security alert
+#rm -rf /system/app/GameCenter;
+rm -rf /system/app/jjcontainer;
+rm -rf /system/app/jjhome;
+rm -rf /system/app/jjknowledge;
+rm -rf /system/app/jjstore;
+rm -rf /system/app/mab;
+rm -rf /system/app/MiLivetalk;
+rm -rf /system/app/Mipay;
+# Can't disable MiuiSuperMarket 
+#rm -rf /system/app/MiuiSuperMarket;
+# Removing causes security alert
+#rm -rf /system/app/MiuiVideo
+rm -rf /system/app/PaymentService;
+rm -rf /system/app/SelfRegister;
+rm -rf /system/app/SystemAdSolution;
+rm -rf /system/app/VoiceAssist;
+rm -rf /system/app/XiaomiVip;
+rm -rf /system/app/XMPass;
+rm -rf /system/priv-app/MiuiVoip;
+rm -rf /system/priv-app/VirtualSim;
+rm -rf /system/priv-app/YellowPage;
+ui_print "    [i] Removed various Chinese-only services";
+ui_print "[#] /system patches...";
+replace_line /system/etc/device_features/$device1.xml "    <bool name=\"support_ota_validate\">true</bool>" "    <bool name=\"support_ota_validate\">false</bool>"
+ui_print "    [i] Disabled OTA app ZIP validation";
+replace_file /system/etc/install_app_filter.xml 644 install_app_filter.xml___replacement;
+ui_print "    [i] Remove Chinese carrier app selection";
+sed 's/^ro.product.locale=*/ro.product.locale=en-US/g' /system/build.prop
+append_file /system/build.prop "# CosmicDan Additionals 01 " build.prop___additions;
+ui_print "    [i] build.prop replacements/additions";
+ui_print "[#] Extracting kernel...";
+dump_boot;
+ui_print "[#] Patching RAMDisk...";
+# change these to fstab functions
+replace_line fstab.mt6797 "/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/system /system ext4 ro wait,verify" "/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/system /system ext4 ro wait"
+ui_print "    [i] Disabled dm-verity (aka verified boot)";
+replace_string fstab.mt6797 "encryptable=" "forceencrypt=" "encryptable="
+ui_print "    [i] Disabled forced userdata encryption";
+append_file init.rc "# init.d" init.rc___additions
+ui_print "    [i] Added init.d support";
+ui_print "[#] Writing kernel with patched RAMDisk...";
+write_boot;
---
+++ busybox_installer.sh
@@ -0,0 +1,273 @@
+#!/sbin/sh
+#BusyBox Installer
+#by YashdSaraf@XDA
+# Modded by CosmicDan for use in CosmicTweaks
+
+OPFD=$1
+BBZIP=$2
+
+#embedded mode support
+readlink /proc/$$/fd/$OPFD 2>/dev/null | grep /tmp >/dev/null
+if [ "$?" -eq "0" ]
+	then
+	OPFD=0
+	for FD in `ls /proc/$$/fd`
+	do
+		readlink /proc/$$/fd/$FD 2>/dev/null | grep pipe >/dev/null
+		if [ "$?" -eq "0" ]
+			then
+			ps | grep " 3 $FD " | grep -v grep >/dev/null
+		  	if [ "$?" -eq "0" ]
+		  		then
+				OPFD=$FD
+				break
+			fi
+		fi
+	done
+fi
+
+( /sbin/mount /data 
+/sbin/mount /cache ) 2>/dev/null
+:
+#Redirect all errors to LOGFILE
+SDCARD=$(ls -d /sdcard 2>/dev/null)
+: ${SDCARD:=$(ls -d /data 2>/dev/null)}
+: ${SDCARD:=/cache}
+LOGFILE=/tmp/BusyBox-YDS-installer.log
+echo "#`date`
+#Ignore umount errors!" > $LOGFILE
+exec 2>>$LOGFILE
+
+ui_print() {
+    echo -e "ui_print $1\n
+    ui_print" >> /proc/self/fd/$OPFD
+	echo -e "$1" >> $LOGFILE
+}
+
+error() {
+	local ERRSTAT=$?
+	if [ $ERRSTAT -ne 0 ]
+		then
+		ui_print "    [!] Error"
+		ui_print "        $1"
+        ui_print " "
+		ui_print "Busybox installer process failed. Check $LOGFILE for errors"
+		[ "$_mounted" == "no" ] && $_sbumt /system
+		exit "$ERRSTAT"
+	else sleep 0.5
+	fi
+}
+
+is_mounted() {
+	grep "$1" /proc/mounts > /dev/null 2>&1
+	return $?
+}
+
+ARCH=arm
+ARCH64=arm64
+
+_sbmt=/sbin/mount
+_sbumt=/sbin/umount
+
+#ui_print "Mounting /system --"
+
+#if (is_mounted /system)
+#	then
+#	$_sbmt -t auto -o rw,remount /system
+#	_mounted="yes"
+#else
+#	$_sbmt -t auto -o rw /system
+#	_mounted="no"
+#fi
+
+#error "    Error while mounting /system"
+
+#ui_print "Checking Architecture --"
+
+FOUNDARCH="$(grep -Eo "ro.product.cpu.abi(2)?=.+" /system/build.prop /default.prop 2>/dev/null | grep -Eo "[^=]*$" | head -n1)"
+
+#ui_print "Looking for => '$ARCH', Found => '$FOUNDARCH'"
+
+
+if [ "${FOUNDARCH::${#ARCH64}}" == "$ARCH64" ]
+	then
+	BBFILE=busybox64
+	#ui_print "64 bit architecture detected --"
+elif [ "${FOUNDARCH::${#ARCH}}" == "$ARCH" ]
+	then
+	BBFILE=busybox
+	#ui_print "32 bit architecture detected --"
+else
+	false
+	error "Wrong architecture found"
+fi
+
+#ui_print "Checking if busybox needs to have SELinux support --"
+
+API=`grep -E "ro.build.version.sdk=.+" /system/build.prop /default.prop 2>/dev/null | grep -Eo "[^=]*$" | head -n1`
+
+SELSTAT="DISABLED"
+[ $API -ge 18 ] && [ -e /sys/fs/selinux/enforce ] && SELSTAT="ENABLED"
+
+for i in /sdcard /data /cache
+do
+	if [ -f $i/bbxselinuxenabled ]
+		then
+		SELSTAT="ENABLED (user override)"
+		break
+	elif [ -f $i/bbxselinuxdisabled ]
+		then
+		SELSTAT="DISABLED (user override)"
+		break
+	fi
+done
+unset i
+
+BBSEL=
+if echo "$SELSTAT" | grep ENABLED > /dev/null 2>&1
+	then
+	BBSEL=-sel
+fi
+
+#ui_print "  "
+#ui_print "SELinux support is $SELSTAT --"
+
+SUIMG=$(ls /data/su.img 2>/dev/null)
+: ${SUIMG:=$(ls /cache/su.img 2>/dev/null)}
+
+if [ ! -z "$SUIMG" ]
+	then
+	ui_print "    [i] Systemless root detected"
+	#Following code to mount su.img is borrowed from supersu update-binary
+	mkdir /su
+	LOOPDEVICE=
+	for LOOP in 0 1 2 3 4 5 6 7
+	do
+		if (! is_mounted /su)
+			then LOOPDEVICE=/dev/block/loop$LOOP
+			if [ ! -f "$LOOPDEVICE" ]
+				then mknod $LOOPDEVICE b 7 $LOOP
+			fi
+			losetup $LOOPDEVICE $SUIMG
+			if [ "$?" -eq "0" ]
+				then
+				mount -t ext4 -o loop $LOOPDEVICE /su
+				if (! is_mounted /su)
+					then /system/bin/toolbox mount -t ext4 -o loop $LOOPDEVICE /su
+				fi
+				if (! is_mounted /su)
+					then /system/bin/toybox mount -t ext4 -o loop $LOOPDEVICE /su
+				fi
+			fi
+			if (is_mounted /su)
+				then break
+			fi
+		fi
+	done
+fi
+
+INSTALLDIR="none"
+for i in /su/xbin /system/xbin
+do
+	if [ -d $i ]
+		then
+		INSTALLDIR="$i"
+		break
+	fi
+done
+unset i
+
+if [ "$INSTALLDIR" == "none" ]
+	then #If xbin was not found in either /su or in /system (;-_-), then create one
+	mkdir /system/xbin
+	chown 0.0 /system/xbin
+	chown 0:0 /system/xbin
+	chmod 0755 /system/xbin
+	INSTALLDIR="/system/xbin"
+fi 2>/dev/null
+
+ui_print "    [#] Removing any old busybox installs..."
+TOTALSYMLINKS=0
+for i in $(ls -d /system /su 2>/dev/null)
+do
+	for j in xbin bin
+	do
+		if [ -e $i/$j/busybox ]
+			then
+			ui_print "        [#] Removing at $i/$j ..."
+			cd $i/$j
+			count=0
+			for k in $(ls | grep -v busybox)
+			do
+				if [ "$k" -ef "busybox" ] || ([ -x $k ] && [ "`head -n 1 $k`" == "#!$i/$j/busybox" ])
+					then
+					rm -f $k
+					count=$((count+1))
+				fi
+			done
+			rm -f busybox
+			cd /
+			error "Error while cleaning BusyBox in $i"
+			TOTALSYMLINKS=$((TOTALSYMLINKS+count))
+		fi
+	done
+done
+unset i
+
+#[ $TOTALSYMLINKS -gt 0 ] && {
+#	ui_print "Total applets removed => $TOTALSYMLINKS --"
+#	ui_print "  "
+#}
+
+BBFILE="${BBFILE}${BBSEL}.xz"
+
+ui_print "    [#] Installing to $INSTALLDIR ..."
+cd $INSTALLDIR
+unzip -o "$BBZIP" $BBFILE ssl_helper xzdec
+error "Error while unzipping $BBZIP to $INSTALLDIR"
+chmod 0555 xzdec
+./xzdec $BBFILE > busybox
+chmod 0555 busybox
+chmod 0555 ssl_helper
+rm $BBFILE
+rm xzdec
+
+#ui_print "Setting up applets --"
+for i in $(./busybox --list)
+do
+	./busybox ln -s busybox $i 2>/dev/null
+	if [ ! -e $i ]
+		then
+		#Make wrapper scripts for applets if symlinking fails
+		echo "#!$INSTALLDIR/busybox" > $i
+		error "Error while setting up applets"
+		chmod 0755 $i
+	fi
+done
+unset i
+
+ui_print "    [#] Adding common system users and groups..."
+unzip -o "$BBZIP" addusergroup.sh
+. ./addusergroup.sh
+rm addusergroup.sh
+
+_resolv=/system/etc/resolv.conf
+if [ ! -f $_resolv ] || ! grep nameserver $_resolv >/dev/null 2>&1
+	then
+	echo "    [#] Adding google nameservers for busybox lookup utils (/system/etc/resolv.conf)" >> $_resolv
+	echo "nameserver 8.8.8.8" >> $_resolv
+	echo "nameserver 8.8.4.4" >> $_resolv
+fi
+
+cd /
+
+#ui_print "Unmounting /system --"
+#ui_print "  "
+if (is_mounted /su)
+	then
+	$_sbumt /su
+	losetup -d $LOOPDEVICE
+	rmdir /su #not using rm -r so /su is deleted only if empty
+fi 2>/dev/null
+#$_sbumt /system
+#ui_print "All DONE! -- Check $LOGFILE for more info"
